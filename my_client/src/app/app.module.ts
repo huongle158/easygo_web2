c4d1d4ed3ea6576386a646698a768ec39200e80b
nytr2808 <truongngocy009@gmail.com>
2022-05-05 10:19:27 +0700

    trang khach san va filter ks

--- my_client/src/app/app.module.ts
+++ my_client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent} from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingComponents
+    RoutingComponents,
+    HeaderComponent,
+    FooterComponent
 
   ],
   imports: [
